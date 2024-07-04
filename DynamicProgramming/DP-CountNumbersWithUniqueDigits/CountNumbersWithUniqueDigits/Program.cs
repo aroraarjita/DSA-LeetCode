7b9bb8576390f399a70d3ab0d421a94decafbb73
Arjita Arora <a.arjita1601@gmail.com>
2024-07-04 15:22:44 +0530

    Dynamic Programming Questions

---
+++ DynamicProgramming/DP-CountNumbersWithUniqueDigits/CountNumbersWithUniqueDigits/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CountNumbersWithUniqueDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 20;
+
+            int result  = NumberCount(a, b);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+           
+        }
+
+        public static int NumberCount(int a, int b)
+        {
+            int count = 0;
+
+            for(int i = a; i < b; i++)
+            {
+                if (isUnique(i))
+                    count++;
+
+            }
+
+            return count;
+
+
+        }
+
+        public static bool isUnique(int number)
+        {
+            HashSet<int> set = new HashSet<int>();
+            var numString = number.ToString();  
+            int len = numString.Length;
+
+            for(int i = 0; i < len; i++)
+            {
+                set.Add(i);
+
+            }
+            return len == 1 || set.Count == len;
+        }
+    }
+}
---
+++ DynamicProgramming/DP-CountingBits/CountingBits/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CountingBits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int[] CountBits(int n)
+        {
+            int[] numberOfOnes = new int[n+1];
+
+            numberOfOnes[0] = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+
+                numberOfOnes[i] = (numberOfOnes[i >> 1]) + (i & 1);
+
+            }
+
+            return numberOfOnes;
+
+        }
+    }
+}
